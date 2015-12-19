9c745e04bd34a5df94f93526a114d871f4771f12
HIGAKI Masaru <mas.higa@gmail.com>
2015-12-19 16:44:10 +0900

    examples

--- README.md
+++ README.md
@@ -7,10 +7,10 @@
 a = (0..9).to_a   # => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 ```
 
-* 各要素を順番に表示しよう。
-* 各要素を文字列にした配列を作ろう。
-* 各要素を 2倍した値を持つ配列を作ろう。
-* 全要素の合計値を計算しよう。
+* 各要素を順番に表示しよう。[回答例](https://github.com/higaki/learn_ruby_kansai_70/blob/master/ex1-1.rb)
+* 各要素を文字列にした配列を作ろう。[回答例](https://github.com/higaki/learn_ruby_kansai_70/blob/master/ex1-2.rb)
+* 各要素を 2倍した値を持つ配列を作ろう。[回答例](https://github.com/higaki/learn_ruby_kansai_70/blob/master/ex1-3.rb)
+* 全要素の合計値を計算しよう。[回答例](https://github.com/higaki/learn_ruby_kansai_70/blob/master/ex1-4.rb)
 
 ### 演習問題 2
 0 から 9 までの数値をもつ配列 a がある。
@@ -18,7 +18,8 @@ a = (0..9).to_a   # => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 a = [*0..9]       # => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 ```
 奇数の要素だけを持つ配列を作ろう。
-ただし `odd?` メソッドは使用禁止。
+ただし `odd?` メソッドは使用禁止。  
+[回答例](https://github.com/higaki/learn_ruby_kansai_70/blob/master/ex2.rb)
 
 ### 演習問題 3
 `Enumerable#map` を自作してみよう。
@@ -29,4 +30,5 @@ module Enumerable
   end
 end
 ```
-ただし `Enumerable#map` と `Enumerable#map!` は使用禁止。
+ただし `Enumerable#map` と `Enumerable#map!` は使用禁止。  
+[回答例](https://github.com/higaki/learn_ruby_kansai_70/blob/master/ex3.rb)
--- ex3.rb
+++ ex3.rb
@@ -2,6 +2,72 @@
 
 module Enumerable
   def my_map
-    # ...
+    unless block_given?
+      # ブロックがもらえなければ Enumerator を返す
+      to_enum __callee__
+    else
+      # 空の Array を用意し
+      inject [] do |result, item|
+        # ブロックの評価結果を追加する
+        result << yield(item)
+      end
+    end
   end
 end
+
+if $0 == __FILE__
+  require 'test/unit'
+  class TestMyMap < Test::Unit::TestCase
+    def setup
+      @array = [*0..9]
+    end
+
+    def test_with_block
+      assert_equal @array.map{|i| i * 2}, @array.my_map{|i| i * 2}
+    end
+
+    def test_with_proc
+      assert_equal @array.map(&:to_s), @array.my_map(&:to_s)
+    end
+
+    def test_without_block
+      actual = @array.my_map
+      @array.each do |expected|
+        assert_equal expected, actual.next
+      end
+      assert_raise StopIteration do
+        actual.next
+      end
+    end
+  end
+
+  class TestMyMapByEmpty < Test::Unit::TestCase
+    def setup
+      @array = []
+    end
+
+    def test_with_block
+      assert_equal [], @array.my_map{|i| i * 2}
+    end
+
+    def test_with_proc
+      assert_equal [], @array.my_map(&:to_s)
+    end
+
+    def test_without_block
+      assert_raise StopIteration do
+        @array.my_map.next
+      end
+    end
+  end
+end
+# >> Loaded suite -
+# >> Started
+# >> ......
+# >> 
+# >> Finished in 0.001017 seconds.
+# >> ------
+# >> 6 tests, 16 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
+# >> 100% passed
+# >> ------
+# >> 5899.71 tests/s, 15732.55 assertions/s
